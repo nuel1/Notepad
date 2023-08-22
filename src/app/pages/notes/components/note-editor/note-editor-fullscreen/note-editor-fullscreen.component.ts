be7f81c6d5aa601b09b4f2bfc79b6e7a9daf5d1e
Joseph Nweke <nwekejoseph52@gmail.com>
2023-08-22 16:23:16 +0100

    changes to fullscreen feature

--- src/app/pages/notes/components/note-editor/note-editor-fullscreen/note-editor-fullscreen.component.ts
+++ src/app/pages/notes/components/note-editor/note-editor-fullscreen/note-editor-fullscreen.component.ts
@@ -4,34 +4,56 @@ import {
   Output,
   EventEmitter,
   OnDestroy,
+  OnInit,
 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Editor, Toolbar, Validators } from 'ngx-editor';
-import { Observable, Subscription, catchError } from 'rxjs';
+import { Observable, Subscription, catchError, combineLatestWith } from 'rxjs';
 
 @Component({
   selector: 'note-editor-fullscreen',
   templateUrl: './note-editor-fullscreen.component.html',
   styleUrls: ['./note-editor-fullscreen.component.scss'],
 })
-export class NoteEditorFullscreenComponent implements OnDestroy {
-  @Input() editor: Editor | any;
+export class NoteEditorFullscreenComponent implements OnInit, OnDestroy {
   @Input() editorToolbarConfig: Toolbar | any;
   @Input() content$: Observable<string> | undefined;
   @Output() onSave = new EventEmitter<string>();
 
-  constructor() {
+  constructor() {}
+
+  subscription: Subscription | undefined;
+  editor: Editor = new Editor();
+  refinedEditorToolbarConfig: Array<string[]> | Toolbar | any = [];
+
+  form = new FormGroup({
+    editorContent: new FormControl('', Validators.required()),
+  });
+
+  ngOnInit(): void {
+    this.removeHeadingTool(this.editorToolbarConfig);
     this.subscription = this.content$?.subscribe(
       (value: string) => this.form.get('editorContent')?.patchValue(value),
       (e) => console.error(e)
     );
   }
 
-  subscription: Subscription | undefined;
-
-  form = new FormGroup({
-    editorContent: new FormControl('', Validators.required()),
-  });
+  removeHeadingTool(editorToolbarConfig: Toolbar) {
+    this.refinedEditorToolbarConfig = editorToolbarConfig.map(
+      (toolbarSet: Array<string[] | any>) => {
+        return toolbarSet.reduce(
+          (
+            tools: Array<string>,
+            tool: string | Record<string, string[]>
+          ): Array<string> => {
+            if (typeof tool === 'string') tools = [...tools, tool];
+            return tools;
+          },
+          []
+        );
+      }
+    );
+  }
 
   saveChanges() {
     const content = this.form.get('editorContent')?.value as string;
